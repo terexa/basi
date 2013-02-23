e5e5a9efc0ae0e0aa217cd1d699538e3c7132c61
Terexa OHiggins <terexa.ohiggins@gmail.com>
2013-02-23 11:49:12 -0300

    Importing my project to Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basi::Application.config.secret_token = '0f8c11f50121c1c61ff6c46596603b2be2103a281b01505fbeee143443cb4a58f58c970b2dddafe42c2b5cdca66f48b98041c99ba0f2f27f6f9d2bb2c1c68dd5'
